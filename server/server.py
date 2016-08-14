bedc3c0761e5c9db69cf6ed3814e34e495ed3e73
Wenzhi Liang <wenzhi.liang@me.com>
2016-08-14 10:45:38 +0800

    relay message with asyncio

--- server/server.py
+++ server/server.py
@@ -3,15 +3,19 @@ from autobahn.asyncio.websocket import \
     WebSocketServerProtocol, WebSocketServerFactory
 
 import asyncio
+import asyncio_redis
 import redis
 import os
 
 _redis_ = os.getenv('REDIS_SVR') or 'localhost'
 
 class BroadcastServerProtocol(WebSocketServerProtocol):
+    def __init__(self, *args, **kwargs):
+        self.loop = asyncio.get_event_loop()
 
     def onOpen(self):
         self.factory.register(self)
+        self.loop.create_task(self.relay_message())
 
     def onMessage(self, payload, isBinary):
         if not isBinary:
@@ -21,15 +25,13 @@ class BroadcastServerProtocol(WebSocketServerProtocol):
         WebSocketServerProtocol.connectionLost(self, reason)
         self.factory.unregister(self)
 
-    def tryRelayMessage(self):
-        if not hasattr(self, 'sub'):
-            self.red = redis.StrictRedis(host=_redis_)
-            self.sub = self.red.pubsub()
-            self.sub.subscribe('chat')
-        m = self.sub.get_message()
-        if m is not None and m['type'] == 'message':
-            print("Relaying %s" % m)
-            self.sendMessage(m['data'])
+    async def relay_message(self):
+        self.red = await asyncio_redis.Connection.create(host=_redis_)
+        self.sub = await self.red.start_subscribe()
+        await self.sub.subscribe(['chat'])
+        while True:
+            msg = await self.sub.next_published()
+            self.sendMessage(msg.value.encode('utf-8'))
 
 
 class BroadcastServerFactory(WebSocketServerFactory):
@@ -52,18 +54,16 @@ class BroadcastServerFactory(WebSocketServerFactory):
     def broadcast(self, msg, client):
         print("broadcasting message '{0}' ..".format(msg))
         self.red.publish('chat', msg.encode('utf-8'))
-        for c in self.clients:
-            self.loop.call_soon(c.tryRelayMessage)
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    factory = BroadcastServerFactory(url=u"ws://0.0.0.0:7000", loop=loop)
+    factory = BroadcastServerFactory(url=u"ws://0.0.0.0:8000", loop=loop)
     factory.protocol = BroadcastServerProtocol
 
-    coro = loop.create_server(factory, '0.0.0.0', 7000)
+    coro = loop.create_server(factory, '0.0.0.0', 8000)
     server = loop.run_until_complete(coro)
-    print("Server running on 7000...")
+    print("Server running on 8000...")
 
     try:
         loop.run_forever()
